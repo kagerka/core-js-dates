9dd72b4dacd5685a9c47f709c48c41e52709bea3
kagerka <kagerka@gmail.com>
2024-03-06 09:59:41 +0300

    feat: add solution to getTime task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -34,8 +34,15 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const fn = (num) => {
+    return num.toString().padStart(2, '0');
+  };
+  const hours = fn(date.getHours());
+  const minutes = fn(date.getMinutes());
+  const seconds = fn(date.getSeconds());
+  const result = `${hours}:${minutes}:${seconds}`;
+  return result;
 }
 
 /**
